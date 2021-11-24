2ccaa76111cd0ecedc471a335bbea6589db88cbd
Kim Chae Eun <kce3615@naver.com>
2021-11-24 14:53:27 +0900

    211123

--- 백준/211123_16953.py
+++ 백준/211123_16953.py
@@ -1,20 +1,21 @@
 import sys
 
 a, b = map(int, sys.stdin.readline().split())
-print(a, b)
 result=0
 while True:
     if b % 2 == 0:
         b = b//2
         result+=1
-    elif b%10 == 1:
+    elif b % 10 == 1:
         b = b//10
         result+=1
+    else:
+        result=-1
+        break
     if b==a:
         result+=1
         break
     elif b<a:
         result=-1
         break
-    print(b)
 print(result)
