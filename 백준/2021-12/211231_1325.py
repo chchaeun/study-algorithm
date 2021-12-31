e610a19ce8a969738f08d39bb3cb95659df7d270
Kim Chae Eun <kce3615@naver.com>
2021-12-31 20:55:33 +0900

    211231 bfs

---
+++ 백준/2021-12/211231_1325.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+
+n, m = map(int, sys.stdin.readline().split())
+
+table = [[] for _ in range(n+1)]
+for i in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    table[b].append(a)
+    
+
+def bfs(i):
+    count=0
+    visited = [False for _ in range(n+1)]
+    visited[i]=True
+    dq = deque([i])
+    while dq:
+        v = dq.popleft()
+        for k in table[v]:
+            if not visited[k]:
+                visited[k] = True
+                count+=1
+                dq.append(k)
+    return count
+
+max_count = -1
+result = []
+
+for i in range(1, n+1):
+    temp = bfs(i)
+    if max_count < temp:
+        result = [i]
+        max_count = temp
+    elif max_count == temp:
+        result.append(i)
+
+print(*result)
