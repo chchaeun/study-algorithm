84c2597ee8b0360cb66329c5385f24c966816dfd
Kim Chae Eun <kce3615@naver.com>
2022-02-21 17:58:33 +0900

    220221

---
+++ 백준/2022-02/220221_11404.py
@@ -0,0 +1,23 @@
+# 플로이드-와샬 알고리즘
+# https://blog.naver.com/ndb796/221234427842
+import sys; input=sys.stdin.readline
+n = int(input().strip())
+m = int(input().strip())
+cost = [[100001 for _ in range(n+1)] for _ in range(n+1)]
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    cost[a][b] = min(cost[a][b], c)
+    
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        if i==k: continue
+        for j in range(1, n+1):
+            if i==j or j==k: continue
+            cost[i][j] = min(cost[i][j], cost[i][k]+cost[k][j])
+for i in cost[1:]:
+    for j in i[1:]:
+        if j==100001:
+            print(0, end=" ")
+        else:
+            print(j, end=" ")
+    print()
