77fcc9d1001e7c274a3a051a4fc174aad3a1ffcc
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2023-05-19 19:59:17 +0900

    1937

---
+++ 백준/2023-04/1937.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+
+n = int(input())
+bamboo = [list(map(int, input().split())) for _ in range(n)]
+
+def in_range(y, x):
+    return 0 <= y < n and 0 <= x < n
+
+def dfs(y, x):
+    if not dp[y][x]:
+        dys, dxs = [1, 0, -1, 0], [0, 1, 0, -1]
+        leaf = True
+        for dy, dx in zip(dys, dxs):
+            ny, nx = y + dy, x + dx
+
+            if in_range(ny, nx) and bamboo[ny][nx] > bamboo[y][x]:
+                dp[y][x] = max(dp[y][x], dfs(ny, nx) + 1)
+                leaf = False
+
+        if leaf:
+            dp[y][x] = 1
+
+    return dp[y][x]
+
+answer = 0
+
+dp = [[0 for _ in range(n)] for _ in range(n)]
+for y in range(n):
+    for x in range(n):
+        answer = max(answer, dfs(y, x))
+
+print(answer)
