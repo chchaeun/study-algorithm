7735fc6919a3287b7f061c4f95244eaceda39f80
김채은 <kce3615@naver.com>
2024-02-08 13:55:04 +0900

    Solve: 11049

--- 백준/2024-02/11049.py
+++ 백준/2024-02/11049.py
@@ -1,20 +1,19 @@
 N = int(input())
 board = [list(map(int, input().split())) for _ in range(N)]
-values = [[0 if i==j else int(1e9) for j in range(N)] for i in range(N)]
+dp = [[0 if i == j else int(1e9) for j in range(N)] for i in range(N)]
 
 for i in range(1, N):
     y, x = 0, i
 
     while 0 <= y < N and 0 <= x < N:
 
-        for i in range(x):
-            if (y + i + 1 > x):
+        for j in range(x):
+            if (y + j + 1 > x):
                 break
-            print(y, y + i, y + i + 1, x)
 
-            values[y][x] = min(values[y][x], values[y][y+i] + values[y+i+1][x] + board[y][0] * board[y+i][1] * board[x][1])
+            dp[y][x] = min(dp[y][x], dp[y][y+j] + dp[y+j+1][x] + board[y][0] * board[y+j][1] * board[x][1])
 
         y += 1
         x += 1
 
-print(values[0][N-1])
+print(dp[0][N-1])
